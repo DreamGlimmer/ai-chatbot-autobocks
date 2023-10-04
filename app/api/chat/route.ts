781a52dbbeb72f1f9169177a13545cd8d4b94dfc
DreamGlimmer <mychildreneson37@gmail.com>
2023-10-03 21:07:15 -0400

    traces

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,6 +2,7 @@ import { kv } from '@vercel/kv'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 
+
 import { auth } from '@/auth'
 import { nanoid } from '@/lib/utils'
 
@@ -12,6 +13,7 @@ const configuration = new Configuration({
 })
 
 const openai = new OpenAIApi(configuration)
+
 
 export async function POST(req: Request) {
   const json = await req.json()
@@ -28,15 +30,21 @@ export async function POST(req: Request) {
     configuration.apiKey = previewToken
   }
 
-  const res = await openai.createChatCompletion({
+  const completionProperties = {
     model: 'gpt-3.5-turbo',
     messages,
     temperature: 0.7,
+  }
+  const res = await openai.createChatCompletion({
+    ...completionProperties,
     stream: true
   })
 
+  const traceId = await createTracer("ai.request", completionProperties)
+
   const stream = OpenAIStream(res, {
     async onCompletion(completion) {
+      await sendEvent("ai.stream.completion", { completion }, traceId)
       const title = json.messages[0].content.substring(0, 100)
       const id = json.id ?? nanoid()
       const createdAt = Date.now()
@@ -60,8 +68,52 @@ export async function POST(req: Request) {
         score: createdAt,
         member: `chat:${id}`
       })
+    },
+    async onStart(){
+      await sendEvent("ai.stream.start", {  }, traceId)
     }
   })
 
   return new StreamingTextResponse(stream)
+}
+
+const createTracer = async(message: string, properties: object)=>{
+  try {
+    const res = await fetch('https://ingest-event.autoblocks.ai', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.AUTOBLOCKS_INGESTION_KEY || ''}`,
+      },
+      body: JSON.stringify({
+        message,
+        properties,
+      }),
+    });
+    const data = await res.json()
+    return data.traceId
+  } catch {
+    console.log("Failed to create tracer.")
+    return ""
+  }
+}
+const sendEvent = async (message: string, properties: object, traceId: string)=>{
+  try {
+    const res = await fetch('https://ingest-event.autoblocks.ai', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.AUTOBLOCKS_INGESTION_KEY || ''}`,
+      },
+      body: JSON.stringify({
+        message,
+        properties,
+        traceId
+      }),
+    });
+    return await res.json();
+  } catch {
+    console.log("Failed to send event to Autoblocks.")
+    return null 
+  }
 }
